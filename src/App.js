61066c22d1f88b5021bce9b4bb6be65b5548dd22
bra-nat <126584626+bra-nat@users.noreply.github.com>
2023-07-18 21:38:49 +0000

    Built Hidden Search App. Learnt useRef, passing styles as props, new CSS techniques and much more.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 // import Alert from './projects/alert app/alert-app.component';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import HiddenSearchBar from './projects/hidden-search-bar-app/hidden-search-bar.component';
 // import TemperatureController from './projects/temperature-controller/temperaturecontroller.component';
 // import LightAndDarkMode from './projects/light-and-dark-mode-app/light-and-dark-mode.component';
 // import LikePhotoApp from './projects/like photo app/like-photo.component';
@@ -11,7 +12,7 @@ import './App.css';
 // import { ThemeProvider } from './context/theme-context/theme-context';
 // import SlideToUnlock from './projects/slide-to-unlock/SlideToUnlock';
 
-import SliderApp from './projects/slider-app/slider-app';
+// import SliderApp from './projects/slider-app/slider-app';
 
 
 function App() {
@@ -34,7 +35,9 @@ function App() {
 
     {/* <SlideToUnlock /> */}
 
-    <SliderApp />
+    {/* <SliderApp /> */}
+
+    <HiddenSearchBar />
       
     </div>
   );
